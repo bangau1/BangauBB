c29f3aa187545ae0847f11bfdde2f18018881ef9
Agung <agp.csui08@gmail.com>
2012-08-08 00:47:46 +0700

    Added myapp.pl

---
+++ myapp.pl
@@ -0,0 +1,30 @@
+#!/usr/bin/env perl
+use Mojolicious::Lite;
+
+# Documentation browser under "/perldoc"
+plugin 'PODRenderer';
+
+get '/' => sub {
+  my $self = shift;
+  $self->render('index');
+};
+
+get '/index.html' => sub {
+  my $self = shift;
+  $self->render(text=>'Hello Agung');
+};
+
+app->start;
+__DATA__
+
+@@ index.html.ep
+% layout 'default';
+% title 'Welcome';
+Welcome to the Mojolicious real-time web framework!
+
+@@ layouts/default.html.ep
+<!DOCTYPE html>
+<html>
+  <head><title><%= title %></title></head>
+  <body><%= content %></body>
+</html>
